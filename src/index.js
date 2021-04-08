b5aa40fc0332ff83b92a4156d56873141b7504f5
suloginscene <suloginscene@gmail.com>
2021-04-09 07:14:06 +0900

    feat: Set redux store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
